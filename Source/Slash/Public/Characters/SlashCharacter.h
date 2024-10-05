58203c963700e2e4ccf682641d41768e481ea274
Shamsul Arifin <arifinbd6@gmail.com>
2024-10-05 21:54:56 +0600

    Added slash character

---
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SlashCharacter.generated.h"
+
+UCLASS()
+class SLASH_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
